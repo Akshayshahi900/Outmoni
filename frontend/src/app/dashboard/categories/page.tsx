685486dd4fad2dcabdc7d217ff144dc8567608e7
Akshayshahi900 <shahiakshay900@gmail.com>
2025-09-15 21:27:02 +0530

    Production bug fixes for ts configs

--- frontend/src/app/dashboard/categories/page.tsx
+++ frontend/src/app/dashboard/categories/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Categories() {
     return (
-        <div>Categories;</div>
+        <div><h1 className='text-6xl text-center text-slate-400'>☹️This page is under developement</h1> </div>
     )
 }
 
