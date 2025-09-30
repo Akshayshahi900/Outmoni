33bf80202f15645d88cde0866fcba73e7a962a8d
Akshayshahi900 <shahiakshay900@gmail.com>
2025-09-30 12:14:38 +0530

    fixes

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U4l8pUpKm5PDSmMe6PbUiGO1aVm6FfQsReuM0mHuue0=\"\n}"
